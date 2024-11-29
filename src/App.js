9bdff39a6747715ac8bf229be24ef823711238dd
v-cshuai <v-cshuai.gd@chinatelecom.cn>
2024-11-30 00:19:00 +0800

    add: lazyLoad

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Demo from './components/1_setState/index'
+import Demo from './components/2_lazyLoad/index'
 
 export default class App extends Component {
   render() {
